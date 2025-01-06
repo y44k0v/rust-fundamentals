a151d4aa16a3209605a33766648b45cc808bb3ae
y44k0v <17897299+y44k0v@users.noreply.github.com>
2025-01-06 02:44:08 -0500

    doing exs

--- examples/3-control-flow/conditionals/src/main.rs
+++ examples/3-control-flow/conditionals/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let proceed = false;
+    let proceed = true;
     if proceed {
         println!("Proceeding");
     } else {
@@ -7,12 +7,16 @@ fn main() {
     }
 
     let height = 190;
-    if height < 180 {
+    if height > 180 && proceed == true {
         println!("Tall");
-    } else if height > 170 {
+    } else if height > 170 && proceed == true{
         println!("Average");
     } else {
+        if proceed == false {
+            println!("Unable to proceeding");
+        } else {
         println!("Short");
+        }
     }
 
 }
